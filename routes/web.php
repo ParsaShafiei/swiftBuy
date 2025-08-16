2bf4dee89cff9de3d2de38db45bbf440c234de44
Parsa Shafiei <parsashafiei1995@gmail.com>
2025-08-16 11:45:04 -0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Admin\Content\CommentController;
+use App\Http\Controllers\Admin\Content\FaqController;
+use App\Http\Controllers\Admin\Content\MenuController;
+use App\Http\Controllers\Admin\Content\PageController;
+use App\Http\Controllers\Admin\Ticket\TicketAdminController;
+use App\Http\Controllers\Admin\Ticket\TicketCategoryController;
+use App\Http\Controllers\Admin\Ticket\TicketController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+require __DIR__ . '/auth.php';
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::prefix('content')->name('content.')->group(function () {
+        Route::resource('menu', MenuController::class);
+        Route::resource('faq', FaqController::class);
+        Route::resource('page', PageController::class);
+        Route::resource('comment', CommentController::class);
+    });
+    Route::prefix('tickets')->name('tickets.')->group(function () {
+        Route::resource('ticket-category', TicketCategoryController::class);
+        Route::resource('ticket-admin', TicketAdminController::class);
+        Route::resource('ticket', TicketController::class);
+    });
+});
